8275a04f1633832e4197367765563a6ef96d47ea
Ankit <ankit871331@gmail.com>
2025-03-30 11:22:35 +0530

    Updated translation logic

--- translator.py
+++ translator.py
@@ -1,8 +1,12 @@
 def translator(phrase):
     translated_phrasee =""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translated_phrasee = translated_phrasee + "g"
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translated_phrasee = translated_phrasee + "G"
+            else:
+                translated_phrasee = translated_phrasee + "g"
+
         else:
             translated_phrasee = translated_phrasee + letter
     return translated_phrasee
